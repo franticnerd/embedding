365edf697ca0090c75554d8657c9d933c7d47d9a
franticnerd <chao.uiuc@gmail.com>
2017-04-14 14:58:20 -0400

    release

--- release/code/dataset.py
+++ release/code/dataset.py
@@ -1,17 +1,6 @@
 from collections import defaultdict
 from tweet_handler import Tweet, POI
 
-# class IO:
-#     def __init__(self):
-#         self.root_dir = '/Users/chao/data/projects/embedding/tweets-dev/'
-#         # if platform == 'darwin' else '/shared/data/kzhang53/embedding/'+paras.pd['dataset']+'/'
-#         self.input_dir = self.root_dir+'input/'
-#         self.output_dir = self.root_dir+'output/'
-#         self.models_dir = self.root_dir+'models/'
-#         self.tweet_file = self.input_dir+'tweets.txt'
-#         self.poi_file = self.input_dir+'pois.txt'
-#         self.case_study_dir = self.output_dir+'case_study/'
-
 
 def read_tweets(tweet_file):
     tweets = []
@@ -36,12 +25,3 @@ def get_voca(tweets, voca_min=0, voca_max=20000):
     return voca
 
 
-def read_pois(poi_file):
-    pois = []
-    for line in open(poi_file):
-        fields = line.strip().split(',')
-        if len(fields)<5:
-            continue
-        poi_id, lat, lng, cat, name = fields[0], float(fields[1]), float(fields[2]), fields[3], ','.join(fields[4:]).lower()
-        pois.append(POI(poi_id, lat, lng, cat, name))
-    return pois
